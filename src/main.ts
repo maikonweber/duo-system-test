bca1a45cf89ee823e30c05b6a253ef59aaba9340
MaikonWeber <maikonweber@gmail.com>
2023-12-19 15:07:15 -0300

    Add documentation swagger and libs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Mutter Corp API')
+    .setDescription('This is Mutter Corporation to access all information')
+    .setVersion('1.0')
+    .addTag('')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
 }
 bootstrap();
