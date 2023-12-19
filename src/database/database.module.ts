0381687d4b5a60ba5cae5b1c3f1296263b13a22b
MaikonWeber <maikonweber@gmail.com>
2023-12-19 15:59:36 -0300

    Add Connection with PostgresSql Database

---
+++ src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres', // Type of your database
+        host: configService.get('DB_HOST'), // Database host
+        port: configService.get('DB_PORT'), // Database port
+        username: configService.get('DB_USERNAME'), // Database username
+        password: configService.get('DB_PASSWORD'), // Database password
+        database: configService.get('DB_NAME'), // Database name
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Auto-create tables based on entities (for development only)
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
