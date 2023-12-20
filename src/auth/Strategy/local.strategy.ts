9fe2b12a3e63210e4fdca6788931531042cff94f
MaikonWeber <maikonweber@gmail.com>
2023-12-20 17:56:12 -0300

    Local Strategy

--- src/auth/Strategy/local.strategy.ts
+++ src/auth/Strategy/local.strategy.ts
@@ -1,24 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from "passport-jwt";
+import { Strategy } from 'passport-jwt';
 import { jwtConstants } from '../jwtConsts';
 import { ExtractJwt } from 'passport-jwt';
 import { AuthService } from '../auth.service';
 
-
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy, 'jwt') {
-  private readonly logger = new Logger(LocalStrategy.name)
-  constructor(private authService : AuthService) {
+  private readonly logger = new Logger(LocalStrategy.name);
+  constructor(private authService: AuthService) {
     super({
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    ignoreExpiration: false,
-    secretOrKey: jwtConstants.secret
-    })
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
   }
 
-  async validate(payload: any) : Promise<any> {
-    console.log(payload.headers)
+  async validate(payload: any): Promise<any> {
+    console.log(payload.headers);
     return payload;
   }
 }
