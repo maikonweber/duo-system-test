6f768386e2fefbf19070b893b0e9d24420fdc66a
MaikonWeber <maikonweber@gmail.com>
2023-12-20 17:55:30 -0300

    Add Auth Initial System many errors

---
+++ src/auth/Strategy/local.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from "passport-jwt";
+import { jwtConstants } from '../jwtConsts';
+import { ExtractJwt } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy, 'jwt') {
+  private readonly logger = new Logger(LocalStrategy.name)
+  constructor(private authService : AuthService) {
+    super({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    ignoreExpiration: false,
+    secretOrKey: jwtConstants.secret
+    })
+  }
+
+  async validate(payload: any) : Promise<any> {
+    console.log(payload.headers)
+    return payload;
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, NotAcceptableException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
+  constructor(
+    private readonly userModule: UserModule,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async loginIn(username: string, password: string) {
+    const users = await this.userModule.getUserAndPassword(username, password);
+    if (!users) throw new NotAcceptableException('could not find the user');
+    const access_token: string = await this.jwtService.signAsync({
+      username: users.username,
+      password: users.password,
+    });
+
+    return {
+      access_token: access_token,
+      usecase: users.usecase,
+    };
+  }
+}
