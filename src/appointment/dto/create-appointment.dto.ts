f88eb4022183fdc4634732959f3c497f4ade2211
MaikonWeber <maikonweber@gmail.com>
2023-12-20 11:02:58 -0300

    Add Dto for all cruds

--- src/appointment/dto/create-appointment.dto.ts
+++ src/appointment/dto/create-appointment.dto.ts
@@ -1,1 +1,17 @@
-export class CreateAppointmentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsInt, IsDate } from 'class-validator';
+import { AppointmentStatus } from '../entities/appointment.enum';
+
+export class CreateAppointmentDto {
+  @ApiProperty()
+  @IsDate()
+  timestamp: Date;
+
+  @ApiProperty({ enum: AppointmentStatus, default: AppointmentStatus.PENDING })
+  @IsEnum(AppointmentStatus)
+  status: AppointmentStatus;
+
+  @ApiProperty()
+  @IsInt()
+  patientId: number;
+}
--- src/patient/dto/create-patient.dto.ts
+++ src/patient/dto/create-patient.dto.ts
@@ -1,1 +1,32 @@
-export class CreatePatientDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsArray,
+  IsDateString,
+} from 'class-validator';
+
+export class CreatePatientDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  cpf: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsDateString()
+  deleted_at?: Date;
+
+  @ApiProperty({ type: [String], required: false })
+  @IsOptional()
+  @IsArray()
+  medical_history?: string[];
+}
