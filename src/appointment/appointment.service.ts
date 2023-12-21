1e562905bf61ec6b745dfdb2607833c3ea73aeb7
MaikonWeber <maikonweber@gmail.com>
2023-12-21 16:05:22 -0300

    Add Tratamento de Erros

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
 import { UpdateAppointmentDto } from './dto/update-appointment.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,68 +13,113 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class AppointmentService {
   private readonly logger = new Logger();
+
   constructor(
     @InjectRepository(Appointment)
     private readonly appointmentRepository: Repository<Appointment>,
   ) {}
-  create(createAppointmentDto: CreateAppointmentDto) {
+
+  async create(createAppointmentDto: CreateAppointmentDto): Promise<void> {
     try {
-      const appointment = this.appointmentRepository.create(createAppointmentDto);
-      this.appointmentRepository.save(appointment);
+      const appointment =
+        this.appointmentRepository.create(createAppointmentDto);
+      await this.appointmentRepository.save(appointment);
     } catch (error) {
-      throw new NotFoundException('Já existe um agendamento com esses dados.');
+      if (error.code === '23505') {
+        // Código de erro 23505 indica violação de restrição única (duplicidade)
+        throw new ConflictException(
+          'Já existe um agendamento com esses dados.',
+        );
+      } else {
+        this.logger.error(
+          `Erro ao criar agendamento: ${error.message}`,
+          error.stack,
+        );
+        throw new Error('Erro interno ao criar agendamento.');
+      }
     }
   }
 
-  findAll() {
-    return this.appointmentRepository.find();
+  async findAll(): Promise<Appointment[]> {
+    try {
+      return await this.appointmentRepository.find();
+    } catch (error) {
+      this.logger.error(
+        `Erro ao buscar agendamentos: ${error.message}`,
+        error.stack,
+      );
+      throw new Error('Erro interno ao buscar agendamentos.');
+    }
   }
 
-  async findOne(id: number): Promise<Appointment | undefined> {
-    const appointment = await this.appointmentRepository.findOne({
-      where: { id: id },
-      relations: ['patient'],
-    });
-    if (!appointment) {
-      throw new NotFoundException();
+  async findOne(id: number): Promise<Appointment> {
+    try {
+      const appointment = await this.appointmentRepository.findOneOrFail({
+        where: { id },
+        relations: ['patient'],
+      });
+      return appointment;
+    } catch (error) {
+      if (error.name === 'EntityNotFoundError') {
+        throw new NotFoundException('Agendamento não encontrado.');
+      } else {
+        this.logger.error(
+          `Erro ao buscar agendamento por ID ${id}: ${error.message}`,
+          error.stack,
+        );
+        throw new Error('Erro interno ao buscar agendamento.');
+      }
     }
-
-    return appointment;
   }
 
   async update(
     id: number,
     updateAppointmentDto: UpdateAppointmentDto,
   ): Promise<Appointment> {
-    const appointment = await this.appointmentRepository.findOne({
-      where: { id: id },
-    });
+    try {
+      const appointment = await this.appointmentRepository.findOneOrFail({
+        where: { id },
+      });
 
-    if (!appointment) {
-      throw new NotFoundException('Appointment not found');
+      if (updateAppointmentDto.timestamp) {
+        appointment.timestamp = new Date(updateAppointmentDto.timestamp);
+      }
+      if (updateAppointmentDto.status) {
+        appointment.status = updateAppointmentDto.status;
+      }
+
+      await this.appointmentRepository.save(appointment);
+      return appointment;
+    } catch (error) {
+      if (error.name === 'EntityNotFoundError') {
+        throw new NotFoundException('Agendamento não encontrado.');
+      } else {
+        this.logger.error(
+          `Erro ao atualizar agendamento ID ${id}: ${error.message}`,
+          error.stack,
+        );
+        throw new Error('Erro interno ao atualizar agendamento.');
+      }
     }
-
-    if (updateAppointmentDto.timestamp) {
-      appointment.timestamp = new Date(updateAppointmentDto.timestamp);
-    }
-    if (updateAppointmentDto.status) {
-      appointment.status = updateAppointmentDto.status;
-    }
-
-    await this.appointmentRepository.save(appointment);
-
-    return appointment;
   }
 
   async delete(id: number): Promise<void> {
-    const appointment = await this.appointmentRepository.findOne({
-      where: { id: id },
-    });
+    try {
+      const appointment = await this.appointmentRepository.findOneOrFail({
+        where: { id },
+      });
 
-    if (!appointment) {
-      throw new NotFoundException('Appointment not found');
+      await this.appointmentRepository.remove(appointment);
+    } catch (error) {
+      if (error.name === 'EntityNotFoundError') {
+        throw new NotFoundException('Agendamento não encontrado.');
+      } else {
+        this.logger.error(
+          `Erro ao excluir agendamento ID ${id}: ${error.message}`,
+          error.stack,
+        );
+        throw new Error('Erro interno ao excluir agendamento.');
+      }
     }
-
-    await this.appointmentRepository.remove(appointment);
   }
 }
