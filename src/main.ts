a8072f8461895f4bd017db448652be0133f1ed9f
MaikonWeber <maikonweber@gmail.com>
2023-12-21 15:37:47 -0300

    Add all project

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
  
   const config = new DocumentBuilder()
-    .setTitle('DuoSyste')
+    .setTitle('DuoSystem')
     .setDescription('DuoSystem Test')
     .setVersion('1.0')
     .addTag('')
