decb66bc3109512b4f517c423b355c9b4292aef2
MaikonWeber <maikonweber@gmail.com>
2023-12-21 08:33:03 -0300

    Add AuthGuard for all appointement e patient route

--- src/appointment/appointment.controller.ts
+++ src/appointment/appointment.controller.ts
@@ -6,32 +6,37 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { AppointmentService } from './appointment.service';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
 import { UpdateAppointmentDto } from './dto/update-appointment.dto';
 import { Appointment } from './entities/appointment.entity';
+import { LocalAuthGuard } from 'src/auth/Guards/local.guards';
 
 @Controller('appointment')
 export class AppointmentController {
   constructor(private readonly appointmentService: AppointmentService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post()
   create(@Body() createAppointmentDto: CreateAppointmentDto) {
     return this.appointmentService.create(createAppointmentDto);
   }
 
-  
+  @UseGuards(LocalAuthGuard)
   @Get()
   findAll() {
     return this.appointmentService.findAll();
   }
 
+  @UseGuards(LocalAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: number): Promise<Appointment | undefined> {
     return this.appointmentService.findOne(id);
   }
 
+  @UseGuards(LocalAuthGuard)
   @Patch(':id')
   async update(
     @Param('id') id: number,
@@ -40,6 +45,7 @@ export class AppointmentController {
     return this.appointmentService.update(id, updateAppointmentDto);
   }
 
+  @UseGuards(LocalAuthGuard)
   @Delete(':id')
   async delete(@Param('id') id: number): Promise<void> {
     this.appointmentService.delete(id);
--- src/patient/patient.controller.ts
+++ src/patient/patient.controller.ts
@@ -5,37 +5,47 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { PatientService } from './patient.service';
 import { CreatePatientDto } from './dto/create-patient.dto';
 import { Patient } from './entities/patient.entity';
 import { UpdatePatientDto } from './dto/update-patient.dto';
+import { LocalAuthGuard } from 'src/auth/Guards/local.guards';
 
 @Controller('patient')
 export class PatientController {
   constructor(private readonly patientService: PatientService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post()
   create(@Body() createPatientDto: CreatePatientDto) {
     return this.patientService.create(createPatientDto);
   }
 
+  @UseGuards(LocalAuthGuard)
   @Get()
   findAll() {
     return this.patientService.findAll();
   }
 
+  @UseGuards(LocalAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: number): Promise<Patient | undefined> {
     return this.patientService.findOne(id);
   }
 
+  @UseGuards(LocalAuthGuard)
   @Patch(':id')
   async update(
     @Param('id') id: number,
     @Body() patientDto: UpdatePatientDto,
   ): Promise<Patient> {
     return this.patientService.update(id, patientDto);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  async delete(@Param() id: number): Promise<void> {
+    return this.patientService.delete(id);
   }
 }
